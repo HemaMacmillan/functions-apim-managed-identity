5f9d6b7fa92dc6f488a156ea56eabd619329b029
Hema <hema_312@hotmail.com>
2025-11-17 14:04:08 -0500

    Update Simple.cs
    1. Removed ALL old namespaces
    2. Added isolated worker namespaces
    3. Replaced signatures
    4. Replaced Newtonsoft.Json → System.Text.Json
    5. Added dependency-injected HttpClient
    6. Rebuilt the response pipeline

--- src/Functions/PublicFunction/Simple.cs
+++ src/Functions/PublicFunction/Simple.cs
@@ -1,83 +1,92 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using System;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
 
 namespace PublicFunction
 {
     public class Simple
     {
-        [FunctionName("simple")]
-        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req, ILogger log)
+        private readonly HttpClient _httpClient;
+        private readonly ILogger _logger;
+
+        public Simple(IHttpClientFactory httpClientFactory, ILoggerFactory loggerFactory)
         {
-            log.LogInformation("Starting function call");
+            _httpClient = httpClientFactory.CreateClient();
+            _logger = loggerFactory.CreateLogger<Simple>();
+        }
 
-            // The client ID of the user assigned managed identity
+        [Function("simple")]
+        public async Task<HttpResponseData> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("Starting function call");
+
             var clientId = Environment.GetEnvironmentVariable("ClientId");
-            
-            // The subscription key (API Key) for Azure API Management (APIM)
-            var apiKey = Environment.GetEnvironmentVariable("ApimKey");
-            
-            // The specific url and route to call in APIM (e.g. https://myapim.azure-api.net/demo/test)
-            var apiUrl = $"{Environment.GetEnvironmentVariable("ApimUrl")}/trusted-simple/test";
-      
-            log.LogInformation($"API Url: {apiUrl}");
+            var apiKey   = Environment.GetEnvironmentVariable("ApimKey");
+            var apiUrl   = $"{Environment.GetEnvironmentVariable("ApimUrl")}/trusted-simple/test";
 
-            var jwt = string.Empty;
+            _logger.LogInformation($"API Url: {apiUrl}");
+
+            string jwt = string.Empty;
+
             try
             {
-                // If we are using a user assigned managed identity (as opposed to a system assigned managed identity) then we must provide the client ID
                 var options = new DefaultAzureCredentialOptions();
-                if(!string.IsNullOrWhiteSpace(clientId))
-                {
-                    options.ManagedIdentityClientId = clientId;
-                }
 
-                // Use the built in DefaultAzureCredential class to retrieve the managed identity, filtering on client ID if user assigned
-                var msiCredentials = new DefaultAzureCredential(options);
-                
-                // Use the GetTokenAsync method to generate a JWT for use in a HTTP request
-                var accessToken = await msiCredentials.GetTokenAsync(new TokenRequestContext(new[] { "https://management.azure.com/.default" }));
-                jwt = accessToken.Token;
-                log.LogInformation("Got the JWT");
+                if (!string.IsNullOrWhiteSpace(clientId))
+                    options.ManagedIdentityClientId = clientId;
+
+                var credential = new DefaultAzureCredential(options);
+
+                var token = await credential.GetTokenAsync(
+                    new TokenRequestContext(new[] { "https://management.azure.com/.default" })
+                );
+
+                jwt = token.Token;
+                _logger.LogInformation("Got the JWT");
             }
             catch (Exception ex)
             {
-                log.LogError(ex, "Error getting token");
+                _logger.LogError(ex, "Error getting token");
             }
-                        
-            var wc = new System.Net.Http.HttpClient();
-            
-            // Add the JWT to the request headers as a bearer token (this is the default for the `validate-azure-ad-token` policy, but you could override it and use a different header)
-            wc.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwt);
-            
-            // Add the Subscription Key (API Key) to the request headers (this is the default header name in APIM, but it can be overridden if needed)
-            wc.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", apiKey);
-            
-            // Call the APIM end point and get the body of the response
-            var result = await wc.GetAsync(apiUrl);
+
+            // Add headers
+            _httpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", jwt);
+
+            _httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", apiKey);
+
+            // Call APIM
+            var result  = await _httpClient.GetAsync(apiUrl);
             var content = await result.Content.ReadAsStringAsync();
-            log.LogInformation("Completed the APIM call");
-            
+
+            _logger.LogInformation("Completed the APIM call");
+
+            var response = req.CreateResponse(result.IsSuccessStatusCode ?
+                                              System.Net.HttpStatusCode.OK :
+                                              System.Net.HttpStatusCode.BadRequest);
+
             if (result.IsSuccessStatusCode)
             {
-                var response = JsonConvert.DeserializeObject<TestResponse>(content);
-                return new OkObjectResult(response);
+                var obj = JsonSerializer.Deserialize<TestResponse>(content,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                await response.WriteAsJsonAsync(obj);
             }
             else
             {
-                log.LogError($"Error making API call: {content}");
-                return new BadRequestObjectResult(content);
+                _logger.LogError($"Error making API call: {content}");
+                await response.WriteStringAsync(content);
             }
+
+            return response;
         }
     }
 }
-
-

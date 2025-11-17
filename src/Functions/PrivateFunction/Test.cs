e223802f5e781abb8a952d37f9e9c86ece25f1be
Hema <hema_312@hotmail.com>
2025-11-17 14:55:33 -0500

    Update Test.cs
    corrected namespace

--- src/Functions/PrivateFunction/Test.cs
+++ src/Functions/PrivateFunction/Test.cs
@@ -4,7 +4,7 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 
-namespace PublicFunction
+namespace PrivateFunction
 {
     public class Test
     {
